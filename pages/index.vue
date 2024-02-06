d8ce7d5610cf5594ec85ecd552137d63856e45e8
Bassam-Z <bassamz.ali@hotmail.com>
2024-02-06 02:09:27 +0100

    Create Nuxt and Pages

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Facilis, quam quaerat. 
+            Distinctio quae odit ex repellat, velit beatae a ducimus?
+        </p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Facilis, quam quaerat. 
+            Distinctio quae odit ex repellat, velit beatae a ducimus?
+        </p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
