db8e8a05915284b8def6fe04d49a8eec542982c7
Bassam-Z <bassamz.ali@hotmail.com>
2024-02-07 01:11:03 +0100

    Link and Layouts

---
+++ pages/products/[id].vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>product details for {{ id }}</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+            Facilis, quam quaerat. 
+            Distinctio quae odit ex repellat, velit beatae a ducimus?
+        </p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+
+    definePageMeta({
+        layout: 'products'
+    })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
